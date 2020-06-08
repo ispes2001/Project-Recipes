70ee8666f27d2f519bbc7bd5dd3834840a760826
Dipesh Thapa <ispes_36@hotmail.com>
2020-06-08 19:24:42 +1000

    due authentications

--- ProjectR/settings.py
+++ ProjectR/settings.py
@@ -143,3 +143,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = ""
+
+#media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     # post views
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     
     path ('login/', auth_views.LoginView.as_view(), name='login'),
     path ('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -15,5 +15,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path ('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
